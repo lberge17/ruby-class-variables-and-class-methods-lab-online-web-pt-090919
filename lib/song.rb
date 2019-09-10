f597e73ce0d076de38c06111455d6f5d78942e21
Laura Berge <lauraeberge@gmail.com>
2019-09-10 20:08:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,35 @@
 class Song
-  attr_accessor :name, :artist, :genre
+  attr_reader :name, :artist, :genre
   @@count = 0
-  
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+  @@artist_count = {}
+ 
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+ 
   def self.count
     @@count
+  end
+  
+  def self.artists
+    @@artists
+  end
+  
+  def self.genres
+    @@genres
+  end
+  
+  def self.genre_count
+    @@genre_count
+  end
+  
+  def self.artist_count
+    @@artist_count
   end
   
 end
